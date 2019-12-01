575227f914b474e5c14ef389b16a69d7c2745954
adsfghnbcvxchjg <2806766160@qq.com>
2019-12-01 10:22:16 +0800

    md

--- work12/index.js
+++ work12/index.js
@@ -1,12 +1,5 @@
-<<<<<<< HEAD
 let year = new Date().getFullYear()
 document.getElementById("cal").innerHTML = calendar(year)
 function setCalendar(yaer){
 
-=======
-let year = new Date().getFullYear()
-document.getElementById("cal").innerHTML = calendar(year)
-function setCalendar(yaer){
-
->>>>>>> 2718bec33d5b3d99154bf2ee71e53206338437f8
 }
